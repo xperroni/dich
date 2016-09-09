17e0d1e464dd4446451dbc6908bda91adb048b70
Helio Perroni Filho <xperroni@gmail.com>
2016-09-09 09:31:28 +0900

    Round number of steering commands up to image stream resolution
    Signed-off-by: Helio Perroni Filho <xperroni@gmail.com>

--- scripts/steerings.py
+++ scripts/steerings.py
@@ -3,13 +3,26 @@
 from re import search
 
 
+Exposure = 5
+
+
 def extract(path):
+    loaded = []
+
     for line in open(path):
         match = search(r'\(([^,]+), ([^\)]+)\)', line)
         if match == None:
             break
 
-        print '[%s, %s]' % (match.group(1), match.group(2))
+        loaded.append('[%s, %s]' % (match.group(1), match.group(2)))
+
+        if len(loaded) < Exposure:
+            continue
+
+        for steering in loaded:
+            print steering
+
+        del loaded[:]
 
 
 def main():
